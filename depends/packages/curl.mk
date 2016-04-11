24ef84899d2b8dcfaa73d2c596750654fdaa4db5
root <dragosbdi@gmail.com>
2016-04-11 13:46:59 +0300

    depends 2

--- depends/packages/curl.mk
+++ depends/packages/curl.mk
@@ -5,20 +5,19 @@ $(package)_file_name=$(package)-$($(package)_version).tar.bz2
 $(package)_sha256_hash=864e7819210b586d42c674a1fdd577ce75a78b3dda64c63565abe5aefd72c753
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared --with-ssl=$(host_prefix)/etc/openssl
-  $(package)_config_opts_linux=--disable-shared
+  $(package)_config_opts=--disable-shared --with-ssl=$(host_prefix)/etc/openssl --prefix=$($(package)_staging_prefix_dir)
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  ./configure $($(package)_config_opts)
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  $(MAKE) 
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) install
+	$(MAKE) install
 endef
 
 define $(package)_postprocess_cmds
--- depends/packages/sqlite3.mk
+++ depends/packages/sqlite3.mk
@@ -5,12 +5,12 @@ $(package)_file_name=sqlite-autoconf-$($(package)_version).tar.gz
 $(package)_sha256_hash=53ecdbb5287af673eca3710c5f6c2305e73bb2d034f2a28770bea2be92ed269e
 
 define $(package)_set_vars
-  $(package)_config_opts=
+  $(package)_config_opts=--prefix=$($(package)_staging_prefix_dir)
   $(package)_config_opts_linux=
 endef
 
 define $(package)_config_cmds
-  $($(package)_autoconf)
+    ./configure $($(package)_config_opts)
 endef
 
 define $(package)_build_cmds
